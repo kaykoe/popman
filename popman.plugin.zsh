b4b7be6c1ba1b7700a6623e043c1711b041aca24
kaykoe <karolkoenigg@gmail.com>
2025-07-24 16:47:52 +0200

    parser wip

--- popman.plugin.zsh
+++ popman.plugin.zsh
@@ -2,22 +2,52 @@
 
 extract_commands() {
   local cmd_line=$1
+  # TODO: subshells in expressions, maybe in double quotes and backticks, maaaaybe in curly braces
+  # TODO: split fields before seding, this will allow easier parsing
   echo "$cmd_line" \
-    | sed "s/'[^']*'//g; s/-\S*//g" \
+    | sed -E 's/(['\''])[^\1]*\1//g
+    s/^(.*[^"' \
+    | sed -E \
+    's/-\S*(\s+[^-]\S*)?//g
+    s/\$\{[^}]*\}//g
+    s/\$\({2}[^\)?=\)]*\){2,}//g
+    s/\$\w+//g' \
+    | tr -d "=!" \
     | xargs \
-    | awk -F'\\$\\(|)|\\|' '{
+    | awk -F'\\$\\(|)|\\||&&|;|{|}' 'BEGIN {
+        empty_or_whitespace = @/^\s*$|^$/
+        has_subcommands["git"]++
+        has_subcommands["apt-get"]++
+        }
+        {
         for (i=1; i<=NF; i++) {
           split($i, a, " ");
-          if (a[1] == "sudo" || a[1] == "xargs" && a[2] != "") {
+          j=1
+          while (j<=length(a)) {
+            if (a[j] in ENVIRON) {
+              delete a[j]
+            }
+            else {
+                j++
+            }
+          }
+      if ((a[1] == "sudo" || a[1] == "xargs") && a[2] !~ empty_or_whitespace) {
             print a[1]
             print a[2]
           }
-          else if (a[1] != "") {
+          else if (a[1] in has_subcommands && a[2] !~ empty_or_whitespace) {
+            print a[1]" "a[2]
+            print a[1]
+          }
+          else if (a[1] !~ empty_or_whitespace) {
             print a[1]
           }
         }
       }' \
-    | uniq \
+    | cat -n \
+    | sort -uk2 \
+    | sort -n \
+    | cut -f2- \
     | tac
 }
 
@@ -37,7 +67,7 @@ popman() {
   elif builtin whence -p "$choice" &>/dev/null && "$choice" --help &>/dev/null; then
     command="$choice --help | less"
   else
-	command="echo 'no manpage or --help available for command: "\"$choice\""' | less"
+    command="echo 'no manpage or --help available for command: "\"$choice\""' | less"
   fi
 
   if [ "${TMUX}" ]; then
